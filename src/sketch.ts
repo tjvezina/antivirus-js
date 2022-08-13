0d34ce4e8f79ad6292affaafcf0d435599a0ba8a
Tyler Vezina <tylerjvezina@gmail.com>
2022-08-13 09:13:49 -0400

    Initial commit

---
+++ src/sketch.ts
@@ -0,0 +1,12 @@
+function windowResized() { // eslint-disable-line no-unused-vars
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() { // eslint-disable-line no-unused-vars
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+}
+
+function draw() { // eslint-disable-line no-unused-vars
+  background(42);
+}
